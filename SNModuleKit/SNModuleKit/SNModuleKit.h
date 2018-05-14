6c2795371a98e843c4d78566d1e95b5f6a6a0937
snlo <snloveydus@sina.com>
2018-05-14 17:06:23 +0800

    update

--- SNModuleKit/SNModuleKit/SNModuleKit.h
+++ SNModuleKit/SNModuleKit/SNModuleKit.h
@@ -9,9 +9,6 @@
 #ifndef SNModuleKit_h
 #define SNModuleKit_h
 
-//#import "SNModuleComponentHeader.h"     //组件
-//#import "SNModuleControlHeader.h"       //可视化控件
-//#import "SNModulePluginHeader.h"        //插件
 #import "SNModuleMiddlewareHeader.h"    //中间件
 #import "SNModuleConfig.h"              //配置
 
