637e4701596b19f5595cf7c5d0a3ab2e9334476b
snlo <snloveydus@sina.com>
2018-05-14 16:05:05 +0800

    添加更新工程模板脚本文件

---
+++ SNModuleKit/SNModuleKit/Resources/setupCodeSnippets
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+cd ~/Desktop
+
+git clone https://github.com/snlo/SNWorkflow.git
+
+cp -rf ~/Desktop/SNWorkflow/SNWorkflow/CodeSnippets ~/Library/Developer/Xcode/UserData
+
+# clear
+rm -rf ~/Desktop/SNWorkflow
+
+echo '设置最新的代码片段。。。成功'
--- SNModuleKit/SNModuleKit/SNModuleKit.h
+++ SNModuleKit/SNModuleKit/SNModuleKit.h
@@ -9,10 +9,10 @@
 #ifndef SNModuleKit_h
 #define SNModuleKit_h
 
-#import "SNModuleComponentHeader.h"     //组件
-#import "SNModuleControlHeader.h"       //可视化控件
+//#import "SNModuleComponentHeader.h"     //组件
+//#import "SNModuleControlHeader.h"       //可视化控件
 #import "SNModuleMiddlewareHeader.h"    //中间件
-#import "SNModulePluginHeader.h"        //插件
+//#import "SNModulePluginHeader.h"        //插件
 #import "SNModuleConfig.h"              //配置
 
 #endif /* SNModuleKit_h */
