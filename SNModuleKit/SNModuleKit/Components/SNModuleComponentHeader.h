ffa370824fa3a8cae9214840c1b1a97cc542dd27
snlo <snloveydus@sina.com>
2018-05-14 17:11:12 +0800

    update

--- SNModuleKit/SNModuleKit/Components/SNModuleComponentHeader.h
+++ SNModuleKit/SNModuleKit/Components/SNModuleComponentHeader.h
@@ -2,7 +2,7 @@
 //  SNModuleComponentHeader.h
 //  SNModuleKit
 //
-//  Created by snlo on 2018/5/11.
+//  Created by snlo on 2018/5/14.
 //  Copyright © 2018年 snlo. All rights reserved.
 //
 
@@ -23,18 +23,16 @@
 #import <SAMKeychain.h>     //账号管理
 #import <Aspects.h>         //面向切面编程
 #import <ReactiveObjC.h>    //响应式编程
-#import <SDWebImageManager.h>  			//图片加载
-#import <UIImageView+WebCache.h>		//UIImageView的扩展
-#import <Masonry.h>						//动态布局
-#import <FMDB.h>						//数据库，基于sql
-#import <Lottie/Lottie.h>				//加载JSON配置的动画，跨平台动画
-#import <pop.h>							//脸书开源动画
-#import <WebViewJavascriptBridge.h>		//UIWebView、WKWebView与H5的交互交接
-#import <MBProgressHUD.h>				//提示框
-#import <MJRefresh.h>					//刷新控件
-#import <AFNetworking.h>				//AFN网络库
-#import <UIKit+AFNetworking.h>			//AFN在UI层的扩展
-
-
+#import <SDWebImageManager.h>              //图片加载
+#import <UIImageView+WebCache.h>        //UIImageView的扩展
+#import <Masonry.h>                        //动态布局
+#import <FMDB.h>                        //数据库，基于sql
+#import <Lottie/Lottie.h>                //加载JSON配置的动画，跨平台动画
+#import <pop.h>                            //脸书开源动画
+#import <WebViewJavascriptBridge.h>        //UIWebView、WKWebView与H5的交互交接
+#import <MBProgressHUD.h>                //提示框
+#import <MJRefresh.h>                    //刷新控件
+#import <AFNetworking.h>                //AFN网络库
+#import <UIKit+AFNetworking.h>            //AFN在UI层的扩展
 
 #endif /* SNModuleComponentHeader_h */
